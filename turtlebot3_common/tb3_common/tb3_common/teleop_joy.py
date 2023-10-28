6eeba99c51d0891db290d08f73a3c20a297feb24
Gai Nakatogawa <nakatogawagai@gmail.com>
2023-10-28 20:55:02 +0900

    fix launch error

---
+++ turtlebot3_common/tb3_common/tb3_common/teleop_joy.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from rclpy.node import Node
+import rclpy
+
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+import traceback
+
+def main():
+    # ノードの初期化
+    rclpy.init()
+    node = Node("tb3_joy_teleop")
+
+    twist = Twist()
+
+    def joy_cb(joy):
+        if joy.buttons[5]:
+            twist.linear.x = joy.axes[1] / 2
+            twist.angular.z = joy.axes[0] / 2
+
+            if joy.buttons[7]:
+                twist.linear.x = joy.axes[1]
+                twist.angular.z = joy.axes[0]
+                
+            cmd_vel_pub.publish(twist)
+
+    # cmd_vel へ publisher
+    cmd_vel_pub = node.create_publisher(Twist, "cmd_vel", 10)
+
+    # joy を subscribe
+    joy_sub = node.create_subscription(Joy, "joy", joy_cb, 10)
+
+    # ノードをスピン
+    rclpy.spin(node)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        rclpy.get_logger("tb3_joy_teleop").info("done")
+    except Exception:
+        traceback.print_exc()
+    finally:
+        rclpy.shutdown()
