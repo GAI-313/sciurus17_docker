93eb07938d2911c8086d58acea5c4f49c46a5bf7
GAI-313 <nakatogawagai@gmail.com>
2024-02-11 22:47:48 +0900

    Fix show traceback bug

--- turtlebot3_common/tb3_common/tb3_common/teleop_joy.py
+++ turtlebot3_common/tb3_common/tb3_common/teleop_joy.py
@@ -12,34 +12,31 @@ def main():
     rclpy.init()
     node = Node("tb3_joy_teleop")
 
-    twist = Twist()
-
-    def joy_cb(joy):
-        if joy.buttons[5]:
-            twist.linear.x = joy.axes[1] / 2
-            twist.angular.z = joy.axes[0] / 2
-
-            if joy.buttons[7]:
-                twist.linear.x = joy.axes[1]
-                twist.angular.z = joy.axes[0]
-                
-            cmd_vel_pub.publish(twist)
-
-    # cmd_vel へ publisher
-    cmd_vel_pub = node.create_publisher(Twist, "cmd_vel", 10)
-
-    # joy を subscribe
-    joy_sub = node.create_subscription(Joy, "joy", joy_cb, 10)
-
-    # ノードをスピン
-    rclpy.spin(node)
-
-if __name__ == "__main__":
     try:
-        main()
+        twist = Twist()
+
+        def joy_cb(joy):
+            if joy.buttons[5]:
+                twist.linear.x = joy.axes[1] / 2
+                twist.angular.z = joy.axes[0] / 2
+
+                if joy.buttons[7]:
+                    twist.linear.x = joy.axes[1]
+                    twist.angular.z = joy.axes[0]
+                    
+                cmd_vel_pub.publish(twist)
+
+        # cmd_vel へ publisher
+        cmd_vel_pub = node.create_publisher(Twist, "cmd_vel", 10)
+
+        # joy を subscribe
+        joy_sub = node.create_subscription(Joy, "joy", joy_cb, 10)
+
+        # ノードをスピン
+        rclpy.spin(node)
+
     except KeyboardInterrupt:
-        rclpy.get_logger("tb3_joy_teleop").info("done")
-    except Exception:
-        traceback.print_exc()
-    finally:
-        rclpy.shutdown()
+        rclpy.logging.get_logger("tb3_joy_teleop").info("Done")
+        
+if __name__ == "__main__":
+    main()
