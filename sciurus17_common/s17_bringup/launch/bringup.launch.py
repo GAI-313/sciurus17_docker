5ca315cd15a238ed637dfce425e757997533a9ff
GAI-313 <nakatogawagai@gmail.com>
2024-02-23 19:04:57 +0900

    first bringup launch

---
+++ sciurus17_common/s17_bringup/launch/bringup.launch.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription 
+from launch.actions import DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource 
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory 
+import traceback
+import os
+
+def generate_launch_description():
+    try:
+        ld = LaunchDescription()
+
+        rviz_config = LaunchConfiguration(
+                'rviz_config',
+                default=os.path.join(
+                    get_package_share_directory('s17_bringup'),
+                    'rviz', 's17_bringup.rviz'
+                )
+            )
+
+        description_bringup = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [
+                    get_package_share_directory(
+                        'sciurus17_gazebo'
+                    )+ '/launch/sciurus17_with_table.launch.py'
+                ]
+            ),
+            launch_arguments={
+                'rviz_config':rviz_config
+            }.items()
+        )
+
+        description_pose_srvs = Node(
+            package='s17_common',
+            executable='pose_srvs',
+        )
+
+        ld.add_action(description_bringup)
+        ld.add_action(description_pose_srvs)
+        
+        return ld
+    except:
+        traceback.print_exc()
