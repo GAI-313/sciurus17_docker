677b9365cd1f3f84ee31e65d9c10c6c1951cdd5f
GAI-313 <nakatogawagai@gmail.com>
2024-02-23 21:47:57 +0900

    Modifired entry.sh

--- entry_exec.sh
+++ entry_exec.sh
@@ -8,8 +8,11 @@ fi
 
 ps_names=$(docker ps --format "{{.Names}}")
 
-if [[ $ps_names == *"$container_name"* ]]; then
+if [[ $ps_names == *"s17"* ]]; then
     echo "Container is already exists"
+    if [[ $ps_names == *"-gpu"* ]]; then
+        container_name="${container_name}-gpu"
+    fi
 else
     echo "execute $container_name"
     docker compose up -d --build $container_name
